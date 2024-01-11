7bacd999fe491fc50e198f7968594cce5faf2932
coderleex <lixinglin@megvii.com>
2024-01-11 14:55:00 +0800

    Feat: add deps and support qt5.15

--- UI/obs-app.cpp
+++ UI/obs-app.cpp
@@ -1328,7 +1328,7 @@ bool LoadBranchesFile(vector<UpdateBranch> &out)
 		goto fail;
 	}
 
-	branchesText = branchesFile.readAll();
+	branchesText = branchesFile.readAll().toStdString();
 	if (branchesText.empty()) {
 		error = "File empty.";
 		goto fail;
